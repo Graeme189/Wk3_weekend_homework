153e1853ce22b831320d9af20442da725af057ec
Graeme Henderson <graeme.189@gmail.com>
2019-07-12 10:17:22 +0100

    Save method added to Customer class

--- console.rb
+++ console.rb
@@ -5,7 +5,13 @@ require_relative('models/ticket')
 require('pry')
 
 customer1 = Customer.new({'name' => 'Lola', 'funds' => 100})
+customer1.save()
 
 customer2 = Customer.new({'name' => 'Dixie', 'funds' => 75})
+customer2.save()
 
 customer3 = Customer.new({'name' => 'Joe', 'funds' => 150})
+customer3.save()
+
+binding.pry
+nil
